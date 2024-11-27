129bb4df7e7c6375bb0cf59a2182c829545fe47f
Tim Hrdy <tim.hrdy.1@gmail.com>
2024-11-27 10:11:50 +0100

    e

--- cart/src/app.js
+++ cart/src/app.js
@@ -1,26 +1,27 @@
-const express = require("express");
-const connectDB = require("../src/config/db"); // Chemin vers votre fichier de configuration
-const swaggerDocs = require("../src/config/Swagger"); // Chemin du fichier swagger.js
+
+const express = require('express');
+const connectDB = require('../src/config/db'); 
+const swaggerDocs = require('../src/config/Swagger');
+const stripe = require('stripe')('sk_test_51QFG6SRsLyr6WwcbLgwLURohiRZbgq0AJv35ZSmnxJoxbQFfWCKs63inmXKYv2T7lsolsyWaZntAodF9spCevW2a00dUlVFAlF'); // Remplacez par votre clé secrète Stripe
 
 const app = express();
-swaggerDocs(app); // Applique la documentation Swagger
+swaggerDocs(app);
 
 // Connexion à la base de données
 connectDB();
 
 // Middleware
-app.use(express.json()); // Pour analyser les requêtes JSON
+app.use(express.json());
 
 // Routes
-app.use("/cart", require("../src/routes/cartRoutes")); // Intégrer vos routes
-app.use("/", (req, res) => {
-  res.send("cart service");
-});
+app.use('/api/cart', require('../src/routes/cartRoutes'));
+app.use('/api/checkout', require('../src/routes/checkoutRoutes')); // Nouvelle route pour le checkout
+
+
 // Démarrer le serveur
 const PORT = 3003;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  console.log(
-    "Swagger documentation available at http://localhost:3003/api-docs"
-  );
+  console.log('Swagger documentation available at http://localhost:3003/api-docs');
+
 });
